a5f198195fa2d5dd052691e281a0f8e9832b0ffc
Mathieu Chartier <mathieuc@google.com>
2015-12-07 09:52:45 -0800

    Add LZ4 makefile targets for device, shared library
    Bug: 22858531
    
    Change-Id: I78574f4e6e7b644f3895425731980af355090d4e

--- lib/Android.mk
+++ lib/Android.mk
@@ -6,5 +6,22 @@ liblz4_src_files := lz4.c lz4hc.c lz4frame.c xxhash.c
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := liblz4
+LOCAL_MULTILIB := both
 LOCAL_SRC_FILES := $(liblz4_src_files)
 include $(BUILD_HOST_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := liblz4
+LOCAL_MULTILIB := both
+LOCAL_WHOLE_STATIC_LIBRARIES := liblz4
+include $(BUILD_HOST_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := liblz4
+LOCAL_SRC_FILES := $(liblz4_src_files)
+include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := liblz4
+LOCAL_WHOLE_STATIC_LIBRARIES := liblz4
+include $(BUILD_SHARED_LIBRARY)
