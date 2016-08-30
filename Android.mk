b4dc00b73a7a49472318e4a5b25c657055fe5e1e
Humberto Borba <humberos@gmail.com>
2016-08-29 22:15:29 -0300

    external: lz4: Add optional lz4 build target
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: Iba414daa6d6c31167371fd93f57c2be4b88df6d6

--- Android.mk
+++ Android.mk
@@ -1,5 +1,49 @@
 # Copyright (C) 2015 The Android Open Source Project
+LOCAL_PATH := $(call my-dir)
 
-LOCAL_PATH:= $(call my-dir)
+common_c_includes := $(LOCAL_PATH)/lib
+common_src_files :=  \
+    lib/lz4.c \
+    lib/lz4frame.c \
+    lib/lz4hc.c \
+    lib/xxhash.c \
+    programs/lz4io.c
 
-include $(call all-makefiles-under,$(LOCAL_PATH))
+include $(CLEAR_VARS)
+LOCAL_MODULE := liblz4
+LOCAL_MULTILIB := both
+LOCAL_SRC_FILES := $(common_src_files)
+LOCAL_C_INCLUDES := $(common_c_includes)
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(common_c_includes)
+include $(BUILD_HOST_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := liblz4
+LOCAL_MULTILIB := both
+LOCAL_WHOLE_STATIC_LIBRARIES := liblz4
+LOCAL_C_INCLUDES := $(common_c_includes)
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(common_c_includes)
+include $(BUILD_HOST_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := liblz4
+LOCAL_SRC_FILES := $(common_src_files)
+LOCAL_C_INCLUDES := $(common_c_includes)
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(common_c_includes)
+include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := liblz4
+LOCAL_WHOLE_STATIC_LIBRARIES := liblz4
+LOCAL_C_INCLUDES := $(common_c_includes)
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(common_c_includes)
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := \
+    programs/bench.c \
+    programs/lz4cli.c
+LOCAL_C_INCLUDES := $(common_c_includes)
+LOCAL_STATIC_LIBRARIES := liblz4
+LOCAL_MODULE := lz4
+include $(BUILD_EXECUTABLE)
